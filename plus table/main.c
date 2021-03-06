66640765dc21e2066fb9193b9f84a40649c07f46
pongdang <pongdang0121@gmail.com>
2021-03-06 20:03:21 +0900

    feat: 덧셈표

---
+++ plus table/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int i;
+	int j;
+	printf("   |");
+	for (i = 1; i < 10; i++) {
+		printf("%3d",i);
+	}
+	printf("\n---+---------------------------\n");
+	for (i = 1; i < 10; i++) {
+		printf("%3d|", i);
+		for (j = 1; j < 10; j++) {
+			printf("%3d", i + j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
